a9758bca72630378cc647faaa03d44a0fd98e818
Dhanashree <suldhanashree13@gmail.com>
2025-05-26 22:37:27 +0530

    All Changes Done

--- Turtle.py
+++ Turtle.py
@@ -1,42 +1,51 @@
 from turtle import Screen, Turtle
 from random import randint, shuffle
 
-src = Screen()
-src.setup(width=600, height=500)
-src.listen()
+screen = Screen()
+screen.setup(width=600, height=500)
 
 colorList = ["red", "orange", "chartreuse", "dark cyan", "dark blue", "dark magenta", "light coral", "light green", "deep pink", "sandy brown"]
+while True:
+    try:
+        tn = int(screen.textinput("Turtle Count", "Enter number of turtles (2–10):"))
+        if 2 <= tn <= 10:
+            break
+    except:
+        pass
 
-tn = int(src.textinput(title="Enter the number of turtles?", prompt="Enter:"))
-bet = src.textinput(title="Turtle on which you want to bet.", prompt="Your bet")
+available_colors = colorList[:tn]
+color_prompt = "\n".join(available_colors)
+while True:
+    bet = screen.textinput("Your Bet", f"Choose one:\n{color_prompt}")
+    if bet in available_colors:
+        break
 
 tur_obj = []
+y_positions = list(range(-100, 100, int(200 / tn)))
+
 for i in range(tn):
-    t = Turtle()
-    t.shape("turtle")
-    t.color(colorList[i])
-    if i % 2 == 0 and i != 0:
-        i = -i
-        t.penup()
-        t.goto(-280, i * 50)
-    else:
-        t.penup()
-        t.goto(-280, i * 50)
-        t.speed(1)
+    t = Turtle(shape="turtle")
+    t.penup()
+    t.color(available_colors[i])
+    t.goto(-280, y_positions[i])
+    t.speed("fastest")
     tur_obj.append(t)
 
-while bet:    
-    shuffle(tur_obj)
-    for i in tur_obj:
-        distance = randint(15, 25)
-        i.forward(distance)
+announce = Turtle()
+announce.hideturtle()
+announce.penup()
+announce.goto(0, 200)
 
-        if i.xcor() > 280:
-            if i.pencolor() == bet:
-                print(f"You win with {i.pencolor()}!")
-                bet = 0
-            else:
-                print(f"You lose! Winning color is {i.pencolor()}.")
-                bet = 0
-        
-src.exitonclick()
+race_on = True
+while race_on:
+    shuffle(tur_obj)
+    for t in tur_obj:
+        t.forward(randint(5, 15))
+        if t.xcor() > 280:
+            winner = t.pencolor()
+            race_on = False
+            message = f"You win! The {winner} turtle won!" if bet == winner else f"You lose! The {winner} turtle won!"
+            announce.write(message, align="center", font=("Arial", 18, "bold"))
+            break
+
+screen.exitonclick()
