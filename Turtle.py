2d74cc397c47d501e45a74154c141362ae1f84b9
Shivprasad08 <shivprasad.mahind@gmail.com>
2025-05-27 18:21:33 +0530

    Issues fixed in the code.

--- Turtle.py
+++ Turtle.py
@@ -3,40 +3,66 @@ from random import randint, shuffle
 
 src = Screen()
 src.setup(width=600, height=500)
+src.title("Turtle Race Game")
 src.listen()
 
 colorList = ["red", "orange", "chartreuse", "dark cyan", "dark blue", "dark magenta", "light coral", "light green", "deep pink", "sandy brown"]
 
-tn = int(src.textinput(title="Enter the number of turtles?", prompt="Enter:"))
-bet = src.textinput(title="Turtle on which you want to bet.", prompt="Your bet")
+# Validate number of turtles
+while True:
+    try:
+        tn = int(src.textinput(title="Enter the number of turtles?", prompt="Enter number between 2 and 10:"))
+        if 2 <= tn <= len(colorList):
+            break
+    except:
+        pass
+    src.textinput("Invalid input", "Please enter a number between 2 and 10. Press Enter to try again.")
+
+# Prepare color options for this race
+shuffle(colorList)
+race_colors = colorList[:tn]
+
+# Validate user's bet
+while True:
+    bet = src.textinput(title="Turtle on which you want to bet.", prompt=f"Choose from: {', '.join(race_colors)}")
+    if bet in race_colors:
+        break
+    src.textinput("Invalid bet", f"Your bet must be one of: {', '.join(race_colors)}. Press Enter to try again.")
+    
 
 tur_obj = []
 for i in range(tn):
     t = Turtle()
     t.shape("turtle")
-    t.color(colorList[i])
-    if i % 2 == 0 and i != 0:
-        i = -i
-        t.penup()
-        t.goto(-280, i * 50)
-    else:
-        t.penup()
-        t.goto(-280, i * 50)
-        t.speed(1)
+    t.color(race_colors[i])
+    t.penup()
+    t.goto(-280, 200 - i * 40)  # Spread turtles vertically
+    t.speed(1)
     tur_obj.append(t)
 
-while bet:    
+# Run the race
+winner = None
+while bet:
     shuffle(tur_obj)
     for i in tur_obj:
         distance = randint(15, 25)
         i.forward(distance)
 
         if i.xcor() > 280:
-            if i.pencolor() == bet:
-                print(f"You win with {i.pencolor()}!")
-                bet = 0
+            winner = i.pencolor()
+            if winner == bet:
+                print(f"You win with {winner}!")
             else:
-                print(f"You lose! Winning color is {i.pencolor()}.")
-                bet = 0
-        
+                print(f"You lose! Winning color is {winner}.")
+            bet = None  # Ends the race
+            break
+
+# Show result on screen
+result = Turtle()
+result.hideturtle()
+result.penup()
+result.goto(0, -200)
+result.write(f"Winner: {winner}\n{'You Win!' if winner == race_colors[race_colors.index(winner)] and winner == winner else 'You Lose!'}",
+             align="center", font=("Arial", 16, "bold"))
+
 src.exitonclick()
